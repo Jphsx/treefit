f8d5628eeb32c25288aed2dfaa3ab1eae108a36d
Jphsx <justin.anguiano1@gmail.com>
2018-07-03 16:34:10 -0500

    debugging more

--- treeFitter/src/treeFitter.cc
+++ treeFitter/src/treeFitter.cc
@@ -407,19 +407,20 @@ OPALFitterGSL* treeFitter::fitParticles(std::vector< std::vector<int>> fit){
 		//save the FOs globally so we can easily
 		//access/print the fitted particles
 		//check fit here directly
-		std::vector<TLorentzVector> fittlv{};
+		std::vector<TLorentzVector*> fittlv{};
 		for(int i=0; i<FO_vec.size(); i++){
 			if(FO_vec.at(i) != NULL){
-			TLorentzVector v();
-			v.SetPxPyPzE(FO_vec.at(i)->getPx(),FO_vec.at(i)->getPy(),FO_vec.at(i)->getPz(),FO_vec.at(i)->getE());
+			TLorentzVector* v = new TLorentzVector();
+			v->SetPxPyPzE(FO_vec.at(i)->getPx(),FO_vec.at(i)->getPy(),FO_vec.at(i)->getPz(),FO_vec.at(i)->getE());
 			fittlv.push_back(v);
 			}
 		}
 		TLorentzVector parent();
 		for(int i=0; i<fittlv.size(); i++){
-			parent += fittlv.at(i);
+			parent += *fittlv.at(i);
 		}
-		std::cout<<"THE PARENT "<<parent.E()<<" "<<parent.M()<<std::endl;;
+		std::cout<<"THE PARENT "<<parent->E()<<" "<<parent->M()<<std::endl;;
+		//Particle::printTLorentzVector(parent);
 		
 
 		 FitObjects = FO_vec;
