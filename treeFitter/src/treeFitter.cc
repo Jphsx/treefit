42a6f7eb79a2b865490db4fb78fda789dd2b476e
Jphsx <justin.anguiano1@gmail.com>
2018-07-03 16:15:07 -0500

    debugging more

--- treeFitter/src/treeFitter.cc
+++ treeFitter/src/treeFitter.cc
@@ -368,8 +368,8 @@ OPALFitterGSL* treeFitter::fitParticles(std::vector< std::vector<int>> fit){
 			if(node->mass != -1){
 				//make a new constraint
 				std::cout<<" THE NODE MASS "<<node->mass<<std::endl;
-				//MassConstraint* mc = new MassConstraint(node->mass);
-				MassConstraint mc(double(node->mass));
+				MassConstraint* mc = new MassConstraint(node->mass);
+				//MassConstraint mc(double(node->mass));
 
 				//get the FOs by iterating over j
 				std::vector<ParticleFitObject*>* mcFitObjects = new vector<ParticleFitObject*>();
@@ -384,11 +384,11 @@ OPALFitterGSL* treeFitter::fitParticles(std::vector< std::vector<int>> fit){
 					if(TFit->recoparts.at( fit.at(i).at(j) )->isTrack){
 						//track push back casted tpfo
 					//	mc->addToFOList(*(TrackParticleFitObject*)FO_vec.at( fit.at(i).at(j) ));
-						mc.addToFOList(*(LeptonFitObject*)FO_vec.at(fit.at(i).at(j) ));
+						mc->addToFOList(*(LeptonFitObject*)FO_vec.at(fit.at(i).at(j) ));
 					}
 					else{
 						//not a track add jfo
-					}	mc.addToFOList(*(JetFitObject*)FO_vec.at( fit.at(i).at(j) ));
+					}	mc->addToFOList(*(JetFitObject*)FO_vec.at( fit.at(i).at(j) ));
 					
 				}//end j
 				//add FOs to constraint
@@ -396,14 +396,14 @@ OPALFitterGSL* treeFitter::fitParticles(std::vector< std::vector<int>> fit){
 				//instead of using a mcvector try just immediately pushing onto the fitter
 				fitter->addConstraint(mc);
 
-fitter->fit();
+
 			}//end if
 		}//end i
 		
 		
 		
 		//do the fit
-		
+		fitter->fit();
 		//save the FOs globally so we can easily
 		//access/print the fitted particles
 		 FitObjects = FO_vec;
