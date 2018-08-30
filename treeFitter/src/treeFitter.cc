69d35e0e4b85775dd7e9b5c46aba2f09969da15f
Jphsx <justin.anguiano1@gmail.com>
2018-08-29 23:09:10 -0500

    debug

--- treeFitter/src/treeFitter.cc
+++ treeFitter/src/treeFitter.cc
@@ -717,7 +717,7 @@ void treeFitter::FindMassConstraintCandidates(LCCollectionVec * recparcol) {
 							newparts.at(fit.at(i).at(k)) = new Particle(newparts.at(fit.at(i).at(k)), newref );
 							//check stop condition have we converged to ref->fitted vertex?
 							std::cout<<"remade the track"<<std::endl;
-							const float* oldref = recoparts.at(fit.at(i).at(k))->track->getReferencePoint();			
+							const float* oldref = TFit->recoparts.at(fit.at(i).at(k))->track->getReferencePoint();			
 							std::cout<<"diff "<< fabs(newref.at(0) -(double) oldref[0])<<std::endl;	
 							std::cout<<"new - old "<< newref.at(0) <<" "<<oldref[0]<<std::endl;	
 									if( fabs(newref.at(0)-(double)oldref[0]) < 1e-2 ){
